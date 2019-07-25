342987a78b54f6739d32608a34ff76d7eb1c1fc2
Rachel Fernandez <ferna185@csusm.edu>
2019-07-25 12:08:26 -0700

    LOOPS

---
+++ WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,28 @@
+const article = {
+  name: 'Dog family gives birth to 10 puppies',
+  views: 1035,
+  datePublished: '03/25/2018',
+  author: {
+    name: 'Rachel Fernandez',
+    title: 'Senior Canine Editor'
+  }
+}
+const foods = {
+  1: 'Pho',
+  2: 'Dim Sum',
+  3: 'Pad Thai',
+  4: 'Ramen',
+  5: 'Street Tacos',
+}
+
+const friends = ['Alice', 'Bob', 'Charlie'];
+
+const classes = [];
+classes.push({course: 'CS 111'. title: 'Computer Science I'});
+classes.push({course: 'Math 160'. title: 'Calculus I'});
+
+const party = {
+  date: '7/4/2018',
+  location: 'Library',
+  guests: ['Mary', 'Joe', 'Smith']
+};
---
+++ WeLearn/M2-javaScript/L4-JSLoop-DOM/loop/loop.js
@@ -0,0 +1,27 @@
+// const names = ['Alice', 'Bob', 'Charlie', 'Deborah', 'Evan'];
+//
+// for (let i = 0; i < 5; i++) {
+//   console.log(names[i]);
+// }
+//
+// names.forEach((name) => {
+//   console.log(name);
+// });
+//
+// let sum = 0;
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+//
+// const findTotal = ((numbers) => {
+//   sum = sum + numbers;
+//   console.log(sum);
+// });
+
+const buttons = document.querySelectorAll('button');
+const box = document.querySelector('#box');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const color = button.innerHTML;
+    box.style.background = color;
+  });
+});
